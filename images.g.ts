ec154f78354b033e34d40d94ef33371e3d426d7a
maryberrypink <157772443+maryberrypink@users.noreply.github.com>
2024-02-01 20:12:31 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "a":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . a a . . . . . . . . . 
+. . . 3 3 3 a . . . . . . . . . 
+. . . 3 3 3 a . . a a . . . . . 
+. . . . . 3 3 3 3 3 a . . . . . 
+. . . . . 3 3 3 3 3 a . . . . . 
+. . . . . 3 . . . 3 . . . . . . 
+. . . . . 3 . . . 3 . . . . . . 
+. . . . . 3 . . . 3 . . . . . . 
+. . . . . a . . . a . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "d":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . a a a . . . . . . . . 
+. . . 3 f 3 3 a . . . . . . . . 
+. . . f 3 3 3 a . . . . a a . . 
+. . . 3 3 3 3 3 3 3 3 3 3 a . . 
+. . . . . 3 3 3 3 3 3 3 3 a . . 
+. . . . . 3 3 3 3 3 3 3 3 a . . 
+. . . . . 3 3 3 . . 3 3 3 a . . 
+. . . . . 3 3 3 . . 3 3 3 . . . 
+. . . . . 3 3 3 . . 3 3 3 . . . 
+. . . . . 3 3 3 . . 3 3 3 . . . 
+. . . . . a a a . . a a a . . . 
+. . . . . a a a . . a a a . . . 
+. . . . . a a a . . a a a . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
